397e6404cd3b7d7752be5d76997f9a86c56a1a81
PavlinaBaldova <pavlina.bal@seznam.cz>
2020-11-21 13:45:32 +0100

    hotovo

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import './index.html';
+import './style.css';
 
-render(
-  <>
-    <header>
-      <h1>čauky </h1>
-    </header>
-    <main>
-      <p>Moje první React stránka</p>
-    </main>
-    <footer>Martin Podloucký</footer>
-  </>,
-  document.querySelector('#app'),
-);
+const App = () => {
+  const [like, setLike] = useState(0);
+  const [dislike, setDislike] = useState(0);
+
+  return (
+    <div className="container">
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
+            <img className="user-avatar" src="/assets/user01.png" />
+            <p className="user-name">Neroxx</p>
+          </div>
+
+          <p className="joke__text">
+            The secret service isn't allowed to yell "Get down!" anymore when
+            the president is about to be attacked. Now they have to yell
+            "Donald, duck!"
+          </p>
+        </div>
+        <div className="joke__likes">
+          <button
+            onClick={() => setLike(like + 1)}
+            id="btn-up"
+            className="btn-like btn-like--up"
+          ></button>
+          <span id="likes-up" className="likes-count likes-count--up">
+            {like}
+          </span>
+          <button
+            onClick={() => setDislike(dislike + 1)}
+            id="btn-down"
+            className="btn-like btn-like--down"
+            src="/assets/img/like-down.png"
+          ></button>
+          <span id="likes-down" className="likes-count likes-count--down">
+            {dislike}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+render(<App />, document.querySelector('#app'));
